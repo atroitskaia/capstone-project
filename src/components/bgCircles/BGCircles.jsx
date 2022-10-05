8c08982177b1f003e8c25680786457c7f9e5511c
Anastassia N Trotiskaia <85711488+atroitskaia@users.noreply.github.com>
2022-10-05 11:17:49 -0400

    First setup layout
    description

---
+++ src/components/bgCircles/BGCircles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './bgCircles.css'
+
+const BGCircles = () => {
+  return (
+    <div>BGCircles</div>
+  )
+}
+
+export default BGCircles
+
---
+++ src/components/urlBox/URLBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './urlBox.css'
+
+const URLBox = () => {
+  return (
+
+    <div>URLBox</div>
+  )
+}
+
+
+export default URLBox
