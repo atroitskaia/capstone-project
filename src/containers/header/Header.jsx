27a95fa368e440e9006dc819716850f024d47d56
Anastassia N Trotiskaia <85711488+atroitskaia@users.noreply.github.com>
2022-10-05 23:35:21 -0400

    last changes
    description

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
+import aboutIllustration from '../../assets/aboutIllustration.svg'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="moodninja__header section__padding">
+      <div className="moodninja__header-content">
+        <h1 className="gradient__text">Sentiment analysis Platform</h1>                        
+        <p> With the help of our AI easily identify positive, negative or neutral sentiment of comments from the range of social media platforms </p>
+      </div>
+
+      <div className="moodninja__header-image">
+        <img src={aboutIllustration} />
+        </div>  
+    </div>
   )
 }
 
