f89d0f17e23c2b80f79a2c98a91ffd12f568d141
Anastassia N Trotiskaia <85711488+atroitskaia@users.noreply.github.com>
2022-10-27 10:41:53 -0400

    Delete index.js

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Analytics } from './analytics/Analytics';
-export { default as Contact } from './contact/Contact';
-export { default as Intro } from './intro/Intro'; 
-export { default as NavBar } from './navBar/NavBar';
-export { default as URLBox } from './urlBox/URLBox';
-
-
