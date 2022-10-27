010f89c2bb62fbccdcffe9a84a73895534e3a498
Anastassia N Trotiskaia <85711488+atroitskaia@users.noreply.github.com>
2022-10-27 10:40:51 -0400

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Contact } from "..";
+import URLBox from "../urlBox/URLBox";
+import './footer.css';
+
+
+
+const Footer = () => {
+  return (
+    <div className="footerCss">
+      <Contact />
+    </div>
+  );
+};
+
+export default Footer;
