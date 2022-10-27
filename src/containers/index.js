52976fd7d9e58a1a4ea11e3b54a579e8a5dd5151
Anastassia N Trotiskaia <85711488+atroitskaia@users.noreply.github.com>
2022-10-27 10:45:08 -0400

    Delete src/containers directory

--- src/containers/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Features } from './features/Features';
-export { default as Footer } from './footer/Footer';
-export { default as Header } from './header/Header';
-export { default as Possibility } from './possibility/Possibility';
-export { default as WhatSentiment } from './whatSentiment/WhatSentiment';
-
-
-
