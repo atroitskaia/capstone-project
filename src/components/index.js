991fecf1ed735cb27565fc944e503f4cad80fd74
Anastassia N Trotiskaia <85711488+atroitskaia@users.noreply.github.com>
2022-11-13 13:49:21 -0500

    removed analytics from index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Analytics } from './analyticsLink/AnalyticsLink';
+// export { default as Analytics } from './analyticsLink/AnalyticsLink';
 export { default as Contact } from './contact/Contact';
 // export { default as Intro } from './intro/Intro'; 
 export { default as NavBar } from './navBar/NavBar';
